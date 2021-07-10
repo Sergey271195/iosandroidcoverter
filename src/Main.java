1025bf3ff5dbb2934ec24b131f3a20dabd057974
Sergey Makovkin <mackovckin.sergei@yandex.ru>
2021-07-10 15:44:11 +0300

    Small tweaks

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,25 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        FilePair filePair = new FilePair("MainPageAndroid.java", "MainPageIos.java").convert();
+        String androidFileName = "MainPageAndroid.java";
+        String iosFileName = "MainPageIos.java";
+        String outputFile = "HybridClass.java";
+
+        FilePair filePair = new FilePair(androidFileName, iosFileName).convert();
+
         List<String> androidFile = filePair.androidFileAsStringList;
         List<String> iosFile = filePair.iosFileAsStringList;
+
         ParsedClass androidConverter = Converter.convertAndroid(androidFile);
-        ParsedClass iosConverter = Converter.convertIos(iosFile);
         System.out.println(androidConverter);
+
+        ParsedClass iosConverter = Converter.convertIos(iosFile);
         System.out.println(iosConverter);
+
         HybridClass hybridClass = HybridClass.convert(androidConverter, iosConverter);
         System.out.println(hybridClass);
 
-        BufferedWriter writer = new BufferedWriter(new FileWriter("HybridClass.java"));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
         writer.write(hybridClass.toString());
         writer.close();
     }
